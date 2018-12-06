956f729915933f812e05fc6ba990bc996d7251a3
Dmitry Vavshko <hivens.mailbox@gmail.com>
2018-12-06 18:49:31 +0200

    add de

--- js/css-switcher.js
+++ js/css-switcher.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
-	$("img#1").click(function() {
-		$("link[media=print]").attr({
-			href: "css/print-color.css"
-		});
-	});
-	$("img#2").click(function() {
-		$("link[media=print]").attr({
-			href: "css/print-color-no.css"
-		});
-	});
-	$("img#3").click(function() {
-		$("link[media=print]").attr({
-			href: "css/print-color-pdf.css"
-		});
-	});
-	$("img[id]").click(function() {
-		$(".active").removeClass("active");
-		$(this).addClass("active")
-	});
+    $("img#1").click(function() {
+        $("link[media=print]").attr({
+            href: "css/print-color.css"
+        });
+    });
+    $("img#2").click(function() {
+        $("link[media=print]").attr({
+            href: "css/print-color-no.css"
+        });
+    });
+    $("img#3").click(function() {
+        $("link[media=print]").attr({
+            href: "css/print-color-pdf.css"
+        });
+    });
+    $("img[id]").click(function() {
+        $(".active").removeClass("active");
+        $(this).addClass("active")
+    });
 });
--- js/to-top-arrow.js
+++ js/to-top-arrow.js
@@ -1,33 +1,33 @@
 (function($, window, document) {
-	$.fn.goTop = function(options) {
-		var defaults = {
-			scrollTop: 100,
-			scrollSpeed: 1000,
-			fadeInSpeed: 1000,
-			fadeOutSpeed: 500
-		};
-		var options = $.extend(defaults, options);
-		var $this = $(this);
-		$(window).on('scroll', function() {
-			if ($(window).scrollTop() > options.scrollTop) {
-				$this.fadeIn(options.fadeInSpeed);
-			} else {
-				$this.fadeOut(options.fadeOutSpeed);
-			}
-		})
-		$this.on('click', function() {
-			$('html,body').animate({
-				'scrollTop': 0
-			}, options.speed)
-		})
-	}
+    $.fn.goTop = function(options) {
+        var defaults = {
+            scrollTop: 100,
+            scrollSpeed: 1000,
+            fadeInSpeed: 1000,
+            fadeOutSpeed: 500
+        };
+        var options = $.extend(defaults, options);
+        var $this = $(this);
+        $(window).on('scroll', function() {
+            if ($(window).scrollTop() > options.scrollTop) {
+                $this.fadeIn(options.fadeInSpeed);
+            } else {
+                $this.fadeOut(options.fadeOutSpeed);
+            }
+        })
+        $this.on('click', function() {
+            $('html,body').animate({
+                'scrollTop': 0
+            }, options.speed)
+        })
+    }
 })(jQuery, window, document)
 
 $(function() {
-	$('#go-top').goTop({
-		scrollTop: 100,
-		scrollSpeed: 1000,
-		fadeInSpeed: 1000,
-		fadeOutSpeed: 500
-	})
+    $('#go-top').goTop({
+        scrollSpeed: 1000,
+        scrollTop: 100,
+        fadeInSpeed: 1000,
+        fadeOutSpeed: 500
+    })
 })
